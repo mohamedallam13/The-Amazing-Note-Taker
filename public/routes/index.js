97dc36645626f7d9e59ae367d0a665f0ca45872b
mohamedallam13 <mohamedallam.tu@gmail.com>
2021-12-03 01:10:06 +0100

    Add Server files and helper files

--- public/routes/index.js
+++ public/routes/index.js
@@ -2,12 +2,28 @@ const express = require('express');
 
 // Import our modular routers for /api and /def
 const apiRouter = require('./api');
-const defRouter = require('./def');
+
+const PORT = process.env.port || 3001;
 
 const app = express();
 
+app.use(express.json());
+app.use(express.static("public"));
+
 app.use('/api', apiRouter);
-app.use('/', defRouter);
-// TODO: Initialize diagnostics route
+
+// GET Route for homepage
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/index.html"))
+);
+
+// GET Route for homepage
+app.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/notes.html"))
+);
+
+app.listen(PORT, () =>
+  console.log(`Express server listening on port ${PORT}!`)
+);
 
 module.exports = app;
--- public/routes/notes.js
+++ public/routes/notes.js
@@ -3,6 +3,7 @@ const { v4: uuidv4 } = require('uuid');
 const {
     readFromFile,
     writeToFile,
+    readAndAppend
 } = require('../helpers/fsUtils');
 
 // GET Route for retrieving all the notes
@@ -10,50 +11,44 @@ const {
 //     readFromFile('./db/tips.json').then((data) => res.json(JSON.parse(data)));
 // });
 
-// GET Route for a specific note
-notesRouter.get('/:note_id', (req, res) => {
-    const noteId = req.params.note_id;
+
+// GET Route for for all notes
+notesRouter.get('/', (req, res) => {
     readFromFile('./db/db.json')
         .then((data) => JSON.parse(data))
         .then((json) => {
-            const result = json.filter((note) => note.note_id === noteId);
-            return result.length > 0
-                ? res.json(result)
-                : res.json('Note not Found');
+            res.json(json)
         });
 });
 
+
+// // GET Route for a specific note
+// notesRouter.get('/:note_id', (req, res) => {
+//     const noteId = req.params.note_id;
+//     readFromFile('./db/db.json')
+//         .then((data) => JSON.parse(data))
+//         .then((json) => {
+//             const result = json.filter((note) => note.note_id === noteId);
+//             return result.length > 0
+//                 ? res.json(result)
+//                 : res.json('Note not Found');
+//         });
+// });
+
 // DELETE Route for a specific note
 notesRouter.delete('/:note_id', (req, res) => {
-    const NoteId = req.params.note_id;
-    readFromFile('./db/db.json')
+    const noteId = req.params.note_id;
+    readFromFile('./db/tips.json')
         .then((data) => JSON.parse(data))
         .then((json) => {
             // Make a new array of all tips except the one with the ID provided in the URL
-            var deleteChek = deleteNote(json, noteId);
-            if (deleteCheck) {
-                res.json(`Item ${noteId} has been deleted 🗑️`);
-                
-            }else{
-                res.json(`Item ${noteId} failed to delete`);
+            const result = json.filter((note) => note.notei_d !== noteId);
 
-            }
-
-            function deleteNote(json, noteId) {
-                const toDelete = [];
-                json.forEach((note, i) => {
-                    if (note.note_id !== noteId) {
-                        toDelete.push(i);
-                    }
-                });
-                if (toDelete.length == 0) return;
-                toDelete.forEach(index=>json.splice(index, 1));
-                // Save that array to the filesystem
-                writeToFile('./db/db.json', json);
-                return true;
-            }
+            // Save that array to the filesystem
+            writeToFile('./db/tips.json', result);
 
             // Respond to the DELETE request
+            res.json(`Note ${tipId} has been deleted 🗑️`);
         });
 });
 
@@ -67,13 +62,8 @@ notesRouter.post('/', (req, res) => {
             text,
             note_id: uuidv4(),
         };
-        readFromFile('./db/db.json')
-            .then((data) => JSON.parse(data))
-            .then((json) => {
-                json.push(newNote)
-                writeToFile(JSON.stringify(notes), './db/db.json');
-                res.json(`note added successfully 🚀`);
-            });
+        readAndAppend(newNote, './db/deb.json');
+        res.json(`note added successfully 🚀`);
     } else {
         res.error('Error in adding note');
     }
